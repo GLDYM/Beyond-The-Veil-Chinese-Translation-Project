a68e0dc3bff042dc68074c27aa0bf86cd27522f3
OddClimate <oddclimate@foxmail.com>
2026-07-03 17:33:32 +0800

    Check.py

--- Check.py
+++ Check.py
@@ -1,7 +1,6 @@
 import json
 import re
 #黑镜键值在这--------->  ^mirror.*?$
-
 while True:
     input_text=input('请利用正则表达式输入待测键值:'+'\n')
     try:
@@ -9,9 +8,6 @@ while True:
         break
     except:
         print("正则表达式无效 请重试")
-
-#此处引用了M.py的代码
-
 old_version = {}
 with open("en_us.lang", "r", encoding="utf-8") as f:
     for line in f:
@@ -22,7 +18,6 @@ with open("en_us.lang", "r", encoding="utf-8") as f:
         if "=" in line:
             key, value = line.split("=", 1)
             old_version[key.strip()] = value.strip()
-
 #保存1.20.1版本文本
 with open (r'en_us.json','r')as f:
     latest_version=json.load(f)
@@ -34,12 +29,11 @@ for key in key_list:
     if result:
         latest_blackmirror_key.append(key)
 #进行核对
-IF_ALL_KEYS_CORRECT=None
+IF_ALL_KEYS_CORRECT=1
 for key in latest_blackmirror_key:
     text=latest_version[key]
     if text in old_version.values():
         print(f'{key} 所对应文本正确')
-        IF_ALL_KEYS_CORRECT=1
     else:
         print(f'错误: {key} 所对应文本错误')
         IF_ALL_KEYS_CORRECT=0
